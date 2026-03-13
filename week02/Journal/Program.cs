1ef476f66daa9bb0fa891b35733a6331f0331bd3
Emeodu-Iheanachor <iheralchi123@gmail.com>
2026-03-13 21:57:49 +0100

    Update for Journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("\nJournal Menu");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("Select a choice: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"\nPrompt: {prompt}");
+                Console.Write("Your response: ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename to save: ");
+                string file = Console.ReadLine();
+                journal.SaveToFile(file);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename to load: ");
+                string file = Console.ReadLine();
+                journal.LoadFromFile(file);
+            }
+        }
+
+        Console.WriteLine("Goodbye!");
     }
 }
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>()
+    {
+        "What was the best part of your day?",
+        "Who was the most interesting person you talked to today?",
+        "What made you smile today?",
+        "What is something you learned today?",
+        "What challenge did you overcome today?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
